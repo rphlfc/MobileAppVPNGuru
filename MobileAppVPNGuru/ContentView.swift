a0caaf086ecc7e4c4d2f1477ee17c35332e29324
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-13 19:30:58 -0300

    Initial Commit

---
+++ MobileAppVPNGuru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileAppVPNGuru
+//
+//  Created by Raphael Cerqueira on 13/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
